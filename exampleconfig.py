ecde3403aae6236d7e603e3e74af45889070a4c4
sourec <126830180+sourec@users.noreply.github.com>
2023-03-03 14:19:18 +0300

    Update exampleconfig.py

--- exampleconfig.py
+++ exampleconfig.py
@@ -4,12 +4,12 @@ from sample_config import Config
 class Development(Config):
     # ضـع اكـوادك تحت مكـان الكـلام العـربي  .. امسـح الكلام العربي اللي بين " " فقـط وضع اكـوادك
     APP_ID = 6 #ضع كود الايبي ايدي الصغيـر مكان الرقم 6 نسخ لصق   
-    API_HASH = "ضع كود الايبي هاش"
-    ALIVE_NAME = "اسم حسابك التلي"
-    DB_URI = "رابـط التخـزين الخـاص بك"
-    STRING_SESSION = "كود تيرمــكس"
-    TG_BOT_TOKEN = "توكـن البـوت الخـاص بك"
-    PRIVATE_GROUP_BOT_API_ID = -100  # ضـع ايـدي كـروب السجـل بجـانب العـدد -100 لا تمسـح العـدد هـذا
+    API_HASH = "93fb093637866a6a054c54ade94a7eff"
+    ALIVE_NAME = "𓆰𓃠˹𝐌𝐎𝐓𝐌𝐑𝐄𝐃𓆪"
+    DB_URI = "postgres://xuoillrh:stonBY6clm4kEsWhh6Ogoe_KWULo9aiu@snuffleupagus.db.elephantsql.com/xuoillrh"
+    STRING_SESSION = "1BJWap1wBuyVjtAj2sfjtCXQNszw8aTxSv-YD29EDQjHZAHbusBAE_VTtlPYy-F9heC2K4ZxyGL0YvtNoR5ZZvzK19M-AYzPPmTmI96UZmF7QTgGRpWcBnJo9xUDDwq1qDX0Cl4u_4s3waDdoy2CLzOHueN2PDmNIhpkT7chu14_jhmRKJT02WAP5LEah1-QWdKWHCo2c2Ar7pP8AUnWDgyORG7aUl5sQPsOWfVkIpPOFlJGMyNKq60oBL7xG7fd04kwdMcUlcAVsMUoir0kFd_qYbHzbQKNBcH00waIpKr36kL6AqJ6-xxT_qOiLlkcAC-cFa7J9eWxhG0OYimwYj1CtVDieneg="
+    TG_BOT_TOKEN = "5675750346:AAH0pJilTlbuKsfyGgHuOenXA5O6AiMJjRc"
+    PRIVATE_GROUP_BOT_API_ID = -1001710688842  # ضـع ايـدي كـروب السجـل بجـانب العـدد -100 لا تمسـح العـدد هـذا
     COMMAND_HAND_LER = "."  # اتركهــا كمـا هـي
     SUDO_COMMAND_HAND_LER = "."  # اتركهــا كمـا هـي
     TZ = "Asia/Baghdad"  # اتركهــا كمـا هـي
